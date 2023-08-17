d47fe2c8be876be8571daf26e52bd1ca6c39242f
Admin <Admin@DESKTOP-UK9TSQ1>
2023-08-17 15:55:36 +0530

    Factorial program

---
+++ validate/src/com/lbrce/devops/validate2.java
@@ -0,0 +1,17 @@
+package com.lbrce.devops;
+
+public class validate2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		  int i,fact=1;  
+		  int number=5;//It is the number to calculate factorial    
+		  for(i=1;i<=number;i++){    
+		      fact=fact*i;    
+		  }    
+		  System.out.println("Factorial of "+number+" is: "+fact);   
+
+	}
+
+}
