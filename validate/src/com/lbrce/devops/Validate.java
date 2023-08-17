d86a0d6071098116c3964b742bf660976e6d9ccf
Admin <Admin@DESKTOP-UK9TSQ1>
2023-08-17 15:34:57 +0530

    validate

---
+++ validate/src/com/lbrce/devops/Validate.java
@@ -0,0 +1,19 @@
+package com.lbrce.devops;
+
+public class Validate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int N = 7;
+		 
+       
+        for (int i = 1; i <= 10; i++) {
+         
+            System.out.println(N + " * " + i + " = "
+                               + N * i);
+
+	}
+
+}
+}
